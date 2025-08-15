992933def4b01c797025a6fb7b4c7cac5681295b
ranagangu <116294485+ranagangu@users.noreply.github.com>
2025-08-15 12:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    // If either string is empty, return empty string
+    if (!str1 || !str2) {
+        return '';
+    }
+    // Return first character of each string combined
+    return str1[0] + str2[0];
 }
+
+// Examples
+console.log(shortcut("Amnesty", "International")); // 'AI'
+console.log(shortcut("Hello", "world"));           // 'Hw'
+console.log(shortcut("", "International"));        // ''
+console.log(shortcut("Amnesty", ""));               // ''
+
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
